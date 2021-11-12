d2246f159e1870cc98cdce44cfd36d549bf42433
Kirti Vernekar <kirtivernekar09@gmail.com>
2021-11-12 23:05:08 +0530

    add homepage and navbar components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import { Homepage, Navbar } from './components';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" component={Homepage}></Route>
+              {/* <Route exact path="/exchanges" component={Exchanges}></Route>
+              <Route exact path="/cryptocurrencies" component={Cryptocurrencies}></Route>
+              <Route exact path="/crypto/:coinId" component={CryptoDetails}></Route>
+              <Route exact path="/news" component={News}></Route> */}
+            </Routes>
+          </div>
+        </Layout>
+        {/* <div className="footer">
+          <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
+            <Link to="/">
+              Cryptoverse Inc.
+            </Link> <br />
+            All Rights Reserved.
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div> */}
+      </div>
     </div>
   );
 }
