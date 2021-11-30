4cca397d08f4f59babbdab889dfd720155722b30
Kirti Vernekar <kirtivernekar09@gmail.com>
2021-11-30 14:48:43 +0530

    rename files

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,6 @@ export { default as Navbar } from './Navbar/Navbar';
 export { default as Loader } from './Loader/Loader';
 export { default as LineChart } from './LineChart/LineChart';
 export { default as CryptoNews } from './CryptoNews/CryptoNews';
-export { default as CryptoCurrencies } from './CryptoCurrencies/CryptoCurrencies';
+export { default as CryptoCurrencies } from './Cryptocurrencies/CryptoCurrencies';
 export { default as Exchanges } from './Exchanges/Exchanges';
 export { default as CryptoDetails } from './CryptoDetails/CryptoDetails';
